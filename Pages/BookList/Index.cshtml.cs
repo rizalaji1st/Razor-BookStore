d308050c281a2c1fe6dfbca7cc73c1ed5c865ccb
rizalaji1st <rizalaji561@gmail.com>
2022-02-11 10:51:55 +0700

    Menambah model, migration, dan persiapan view untuk book

---
+++ Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Razor_BookStore.Models;
+
+namespace Razor_BookStore.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public async void OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+    }
+}
