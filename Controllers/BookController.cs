2c5d32c9125d189257e65ac30ed074554f8bfa2e
rizalaji1st <rizalaji561@gmail.com>
2022-02-15 13:24:28 +0700

    Menambah support web API

---
+++ Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor_BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor_BookStore.Controllers
+{
+    [Route("api/book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _db.Book.ToList() });
+        }
+    }
+}
